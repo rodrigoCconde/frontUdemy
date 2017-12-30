06606d575e93daa41b5604fde98033c042a83851
rodrigoCconde <conde.web@gmail.com>
2017-12-30 16:44:30 -0200

    Arquivos iniciais do projeto

--- backend/api/billingSummary/billingSummaryService.js
+++ backend/api/billingSummary/billingSummaryService.js
@@ -7,7 +7,7 @@ function getSummary(req, res) {
   BillingCycle.aggregate({
     $project: {credit: {$sum: "$credits.value"}, debt: {$sum: "$debts.value"}}
   }, {
-    $group: { id: null, credit: {$sum: "$credit"}, debt: {$sum: "debt"}}
+    $group: { _id: null, credit: {$sum: "$credit"}, debt: {$sum: "debt"}}
   }, {
       $project: { _id: 0, credit: 1, debt: 1 }
   }, function(error, result){
