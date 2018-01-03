5d8d3e7138505b37d507454615d97de75b603016
rodrigoCconde <conde.web@gmail.com>
2018-01-02 22:09:15 -0200

    Arquivos factory e controller

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -1,18 +1,19 @@
 (function(){
   angular.module('primeiraApp').controller('BillingCycleCtrl', [
     '$http',
+    'msgs',
     BillingCycleController
   ])
-  function BillingCycleController($http){
+  function BillingCycleController($http, msgs){
     const vm = this
 
     vm.create = function(){
       const url = 'http://localhost:3003/api/billingCycles'
       $http.post(url, vm.billingCycle).then(function(response){
         vm.billingCycle = {}
-        console.log('Sucess!')
+        msgs.addSuccess('Operação realizada com sucesso!')
       }).catch(function(response){
-        console.log('algo deu errado! :(')
+        msgs.addError(response.data.errors)
       })
     }
   }
